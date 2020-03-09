2182ece4705b07f0acb3531bbcd08cf9067616d9
robosophus <robosophus@nomail.invalid>
2020-03-09 23:49:14 +0100

    added support for firmware version 1886

--- patcher.py
+++ patcher.py
@@ -12,7 +12,13 @@ data = bytearray(data);
 patterns = [b"\xF2\x04\x03\xFF\xF7\x4B\xFF", b"\xF2\x04\x03\xFF\xF7\x45\xFF", b"\x33\x46\x4B\xA8\x10\x22"]
 replaces = [b"\xF2\x04\x03\xE3\x20\x4B\xFF", b"\xF2\x04\x03\xE3\x20\x45\xFF", b"\x33\x46\x47\xA8\x10\x22"]
 
-matches = [0,0,0];
+patterns1886 = [b"\x10\x22\x01\x21\x4B\xA8\xF8\xF7\xC0\xEA", b"\x01\x21\x4B\xA8\xF8\xF7\xA8\xEA\x01\xA8", b"\xFF\xF7\x44\xFF\x03\xAA\x31\x46\x38\x46"]
+replaces1886 = [b"\x10\x22\x01\x21\x47\xA8\xF8\xF7\xC0\xEA", b"\x01\x21\x47\xA8\xF8\xF7\xA8\xEA\x01\xA8", b"\xAF\xF3\x00\x80\x03\xAA\x31\x46\x38\x46"]
+
+patterns += patterns1886
+replaces += replaces1886
+
+matches = [0,0,0,0,0,0];
 for i in range(len(patterns)):
    pattern = patterns[i]
    replace = bytearray(replaces[i])
@@ -24,7 +30,10 @@ for i in range(len(patterns)):
        print ("Patched  %08X: "%offset, ', '.join("%02X"%(ch) for ch in replace))
        data[offset:offset+len(pattern)] = replace
 
-if( ((matches[0] == 1 and matches[1] == 0) or (matches[0] == 0 and matches[1] == 1)) and matches[2] == 1):
+success = ((matches[0] == 1 and matches[1] == 0) or (matches[0] == 0 and matches[1] == 1)) and matches[2] == 1
+success1886 = matches[3]==1 and matches[4]==1 and matches[5]==1
+
+if(success or success1886):
    print("Patch appears to be successful. Writing to outfile")
    with open(outfile, "wb") as f:
       f.write(data)
